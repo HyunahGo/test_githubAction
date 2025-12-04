812bdbdd58000dd0cecdf26788ecd5ec0005c1c8
HyunahGo <gusdkrh23@duksung.ac.kr>
2025-12-04 10:03:53 +0900

    thrid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // computes a factorial of a given positive integer
 function factorial(n) {
-  /*
-    if( n < 0 ) {
-        throw new Error('n must be a positive integer');
-    }
-        */
+  if (n < 0) {
+    throw new Error("n must be a positive integer");
+  }
 
   if (n === 0) {
     return 1;
